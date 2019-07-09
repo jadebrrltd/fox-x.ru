1122668cedb7d23d925dcd148f1c66ccbdfb1cc2
osipovia <osipovia85p@gmail.com>
2019-07-09 18:21:01 +0200

    Добавил Yandex.Metrika counter

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -18,7 +18,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta property="og:type" content="website"/>
     <meta property="og:url" content="//fox-x.ru/"/>
-    <meta property="og:title" content="Официальный сайт Фокс икс">
+    <meta property="og:title" content="Официальный сайт Фокс икс!!!">
     <meta property="og:description" content="Испытай удачу !"/>
     <meta property="og:site_name" content="FOX-X"/>
     <meta property="og:image" content="//fox-x.ru/img/fox_prewiev_vk.jpg"/>
@@ -34,6 +34,35 @@
     @else
         <link class="main-css" rel="stylesheet" href="{{ asset('css/main.min.css') }}">
     @endif
+
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript" >
+        (function (d, w, c) {
+            (w[c] = w[c] || []).push(function() {
+                try {
+                    w.yaCounter53248024 = new Ya.Metrika({
+                        id:53248024,
+                        clickmap:true,
+                        trackLinks:true,
+                        accurateTrackBounce:true
+                    });
+                } catch(e) { }
+            });
+
+            var n = d.getElementsByTagName("script")[0],
+                s = d.createElement("script"),
+                f = function () { n.parentNode.insertBefore(s, n); };
+            s.type = "text/javascript";
+            s.async = true;
+            s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+            if (w.opera == "[object Opera]") {
+                d.addEventListener("DOMContentLoaded", f, false);
+            } else { f(); }
+        })(document, window, "yandex_metrika_callbacks");
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/53248024" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
 
 </head>
 <body @if(checkSessionTheme()) class="body--dark" @endif>
