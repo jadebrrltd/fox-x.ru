5ee2ef9d00e9f5e56b6c5594bd82a8e6b54fac72
Elijah Doman <gshkvbmstu@gmail.com>
2020-01-13 00:26:03 +0000

    up

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
+import argparse
 from aiohttp import web
 from router import routes
+
+
+parser = argparse.ArgumentParser(description="aiohttp server example")
+parser.add_argument('--path')
+parser.add_argument('--port')
 
 app = web.Application()
 
 
-for route in routes:
-    app.router.add_route(**route)
 
-web.run_app(app)
+if __name__ == '__main__':
+    app = web.Application()
+
+    args = parser.parse_args()
+    for route in routes:
+        app.router.add_route(**route)
+    web.run_app(app, path=args.path, port=args.port)
