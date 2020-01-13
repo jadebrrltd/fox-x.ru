bba33e8e08cf55c56e7cb062ec411288f7fbbefb
Elijah Doman <gshkvbmstu@gmail.com>
2020-01-13 22:38:44 +0000

    Исправление ошибок в админке Crash без дебаг режима

--- resources/views/admin/crash_result.blade.php
+++ resources/views/admin/crash_result.blade.php
@@ -1,42 +1,43 @@
 <div class="left">
     @if($mode == 0)
-        @if($info->id)
+        @if(isset($info->id))
             <div class="info">
                 Игра №<b>{{$info->id}}</b>
             </div>
+            <div class="total">
+                @if(isset($info->rand_number) and $info->rand_number > 0)
+                    @php
+                        if($info->rand_number < 10) $co = 0.2;
+                        elseif($info->rand_number >= 10) $co = 1;
+                        elseif($info->rand_number >= 20) $co = 2;
+                        elseif($info->rand_number >= 50) $co = 4;
+                        elseif($info->rand_number >= 60) $co = 8;
+                        elseif($info->rand_number >= 90) $co = 10;
+                        elseif($info->rand_number >= 150) $co = 20;
+                        elseif($info->rand_number >= 250) $co = 30;
+                        $rand_number = $info->rand_number + $co;
+                    @endphp
+                    ~<snan id="total_x" title="Текущий коэфициент">{{number_format((float)$rand_number, 2, '.', '')}}</snan>X
+                @else
+                    <snan id="total_x" title="Текущий коэфициент">0.00</snan>X
+                @endif
+                <small id="total_x" style="color:
+                        @if($info->profit)
+                            #1f92bf
+                        @else
+                            red
+                        @endif
+                    " title="Профит
+                игры">{{number_format(
+                (float)$info->profit, 2,
+                '.',
+                         '')
+                        }}X</small>
+            </div>
         @endif
-        <div class="total">
-            @if(isset($info->rand_number) and $info->rand_number > 0)
-                @php
-                    if($info->rand_number < 10) $co = 0.2;
-                    elseif($info->rand_number >= 10) $co = 1;
-                    elseif($info->rand_number >= 20) $co = 2;
-                    elseif($info->rand_number >= 50) $co = 4;
-                    elseif($info->rand_number >= 60) $co = 8;
-                    elseif($info->rand_number >= 90) $co = 10;
-                    elseif($info->rand_number >= 150) $co = 20;
-                    elseif($info->rand_number >= 250) $co = 30;
-                    $rand_number = $info->rand_number + $co;
-                @endphp
-                ~<snan id="total_x" title="Текущий коэфициент">{{number_format((float)$rand_number, 2, '.', '')
-                        }}</snan>X
-            @else
-                <snan id="total_x" title="Текущий коэфициент">0.00</snan>X
-            @endif
-            <small id="total_x" style="color:
-                    @if($info->profit)
-                        #1f92bf
-                    @else
-                        red
-                    @endif
-                " title="Профит
-            игры">{{number_format(
-            (float)$info->profit, 2,
-            '.',
-                     '')
-                    }}X</small>
-        </div>
-        <div class="info">Онлайн в Crash: {{count($bets)}}</div>
+        @if(isset($bets))
+            <div class="info">Онлайн в Crash: {{count($bets)}}</div>
+        @endif
         <script>
             var co = 0;
             var intS  = 0;
@@ -99,10 +100,12 @@
         </script>
         <script>
 
-            var co = 5;
-            var max = <?= $info->profit; ?>;
-            var co = <?= $info->number; ?>;
-            var i = <?= $info->rand_number; ?>;
+            @if(isset($info->id))
+                var co = 5;
+                var max = <?= $info->profit; ?>;
+                var co = <?= $info->number; ?>;
+                var i = <?= $info->rand_number; ?>;
+            @endif
             var step = 0.1;
             var pr, x = 0, y, drawY;
 
@@ -151,10 +154,10 @@
         <button onclick="stop_game();" class="btn btn-stop-game hidden">Остановить график?</button>
 
 </div>
-
 <div class="right">
-@if(isset($bets))
-        @if(count($bets))
+    @if(isset($bets))
+        @if(!$bets->isEmpty())
+            {{dd($bets)}}
             <div class="table-bets">
                 <table class="table table-hover" style="width: 100%;">
                     <thead>
