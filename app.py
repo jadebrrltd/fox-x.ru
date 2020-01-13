7d526f79880b2d62227e769f84e18aa69e65b53a
Elijah Doman <gshkvbmstu@gmail.com>
2020-01-13 00:26:16 +0000

    Up

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import argparse
+import logging
 from aiohttp import web
 from router import routes
+
 
 
 parser = argparse.ArgumentParser(description="aiohttp server example")
@@ -10,11 +12,11 @@ parser.add_argument('--port')
 app = web.Application()
 
 
-
 if __name__ == '__main__':
     app = web.Application()
 
     args = parser.parse_args()
     for route in routes:
         app.router.add_route(**route)
+    logging.basicConfig(level=logging.DEBUG)
     web.run_app(app, path=args.path, port=args.port)
